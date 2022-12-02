43ab45ee2f46d49f3e600cf4bdb8cf1fdbafe61d
Arminnn-droid <arminmalekm44@gmail.com>
2022-12-02 11:21:28 +0330

    well, fact funk done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+    }
+    public static double fact(double number){
+        double res = 1;
+        if (number> 1){
+            for(double x = 1; x <= number; x++){
+                res *=  x;
+            }
+        }
 
+        return res;
+    }
+
+    public static double macSin(double tetta, double k){
+        double res = 0;
+
+        for(double x= 0; x<=k; x++){
+            double formulaRez = 4;
+            res = res + res;
+        }
     }
 }

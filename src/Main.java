0d26fc54ee56d93ae774cdf174fef53168139dd2
Arminnn-droid <arminmalekm44@gmail.com>
2022-12-02 12:46:16 +0330

    well, Done Done Done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+/*
+
+                   ArminMalekMohammadiBidhendi     39912330117022    The encrypt decrypt practice
+
+ */
+
 public class Main {
     public static void main(String[] args) {
+        double k = 20;
+        double x = 14;
+        final  double pi = 3.14;
+        double tetta = 0;
+        double radian = (tetta/180)*pi;
+
+
+        // sin!
+        System.out.println("Sin! | " + Math.sin(radian) + " | " + k + " | " + macSin(tetta,k));
+
+        //cos!
+        System.out.println("Cos! | " + Math.cos(radian) + " | " + k + " | " + macCos(tetta,k));
+
+        //ln(x)
+        System.out.println("Ln! | " + Math.log(x) + " | " + k + " | " + macLn(x,k));
+
+
     }
     public static double fact(double number){
         double res = 1;
@@ -14,10 +37,42 @@ public class Main {
 
     public static double macSin(double tetta, double k){
         double res = 0;
+        final  double pi = 3.14;
+        double radian = (tetta/180)*pi;
 
-        for(double x= 0; x<=k; x++){
-            double formulaRez = 4;
-            res = res + res;
+        for(double n= 0; n<=k; n++){
+            double formulaRez = (Math.pow(-1, n))*((Math.pow(radian,(2*n)+1)/
+                                                   (fact((2*n)+1))));
+            res += formulaRez;
         }
+
+        return res;
+    }
+
+    public static double macCos(double tetta, double k){
+        double res = 0;
+        final  double pi = 3.14;
+        double radian = (tetta/180)*pi;
+
+        for(double n= 0; n <= k; n++) {
+            double formulaRes = ((Math.pow(-1,n))*(Math.pow(radian,2*n))/
+                                                  (fact((2*n))));
+
+            res += formulaRes;
+        }
+
+
+        return res;
+    }
+
+    public static double macLn(double x, double k){
+        double res = 0;
+
+        for(double n = 1; n <= k; n++){
+            double formulaRes = (Math.pow((x-1),n)/n)*Math.pow(-1, n+1);
+            res += formulaRes;
+        }
+
+        return res;
     }
 }
